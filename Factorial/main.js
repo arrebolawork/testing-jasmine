475657dda41752f720f65fea573b2ed76ddba234
David Arrebola <arrebola.work@gmail.com>
2025-05-09 17:57:17 +0200

    añadida la condicion si es negativo

--- Factorial/main.js
+++ Factorial/main.js
@@ -1,6 +1,6 @@
 function factorial(n) {
   if (n === 0) return 1;
-
+  if (n < 0) throw new Error("Debes ingresar un número positivo");
   let resultado = 1;
   for (let i = 1; i <= n; i++) {
     resultado *= i;
--- Factorial/main.spec.js
+++ Factorial/main.spec.js
@@ -1,25 +1,28 @@
 describe("factorial", function () {
-  it("debe devolver 1 si el número es 0", function () {
+  it("debe devolver 1 si el número es 0", () => {
     expect(factorial(0)).toBe(1);
   });
 
-  it("debe devolver 1 si el número es 1", function () {
+  it("debe devolver 1 si el número es 1", () => {
     expect(factorial(1)).toBe(1);
   });
 
-  it("debe devolver 2 si el número es 2", function () {
+  it("debe devolver 2 si el número es 2", () => {
     expect(factorial(2)).toBe(2);
   });
 
-  it("debe devolver 6 si el número es 3", function () {
+  it("debe devolver 6 si el número es 3", () => {
     expect(factorial(3)).toBe(6);
   });
 
-  it("debe devolver 24 si el número es 4", function () {
+  it("debe devolver 24 si el número es 4", () => {
     expect(factorial(4)).toBe(24);
   });
 
-  it("debe devolver 120 si el número es 5", function () {
+  it("debe devolver 120 si el número es 5", () => {
     expect(factorial(5)).toBe(120);
+  });
+  it("debe devolver un error con mensaje si el numero es negativo", () => {
+    expect(() => factorial(-1)).toThrow(new Error("Debes ingresar un número positivo"));
   });
 });
